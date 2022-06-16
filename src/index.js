8e6cf7a25fc0a91985aa2662f882feb07b959c7b
rbgreenway3rd <3rdgreenway@gmail.com>
2022-06-16 18:32:05 -0500

    basic Apollo and gql functionality achieved.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const HASURA_SECRET =
+  "w39uMeRGp6YDm5AOUZk02c9iIULiH5BUEeru6SfI4Q2y6q86ZUb3uyHZX3RwTUHC";
+
+const client = new ApolloClient({
+  uri: "https://graphql-heroku-api.hasura.app/v1/graphql",
+  cache: new InMemoryCache(),
+  headers: {
+    "x-hasura-admin-secret": HASURA_SECRET,
+  },
+});
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>
+  </ApolloProvider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
